8a6c111a1dbddfc57a599dbbb9e0cd0c1bccc66f
Jonathan Bovee <jbovee99@gmail.com>
2019-04-28 00:15:44 -0500

    second commit

--- app/src/main/java/com/example/uvrisk/MainActivity.java
+++ app/src/main/java/com/example/uvrisk/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.uvrisk;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +21,8 @@ import org.json.JSONObject;
 public class MainActivity extends AppCompatActivity {
     private static final String APIKEY = "ab2c57df8757b2533797c8412820d3b9";
     private static RequestQueue requestQueue;
+    private static String LOG = "UVRISK";
+    private static final String nonIntegerError = "Error: Make sure your Latitude and Longitude are numbers";
     /*
     URL FOR API CALL http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}
      */
@@ -39,23 +42,35 @@ public class MainActivity extends AppCompatActivity {
                 double latValue = 0;
                 double lonValue = 0;
                 try {
-                    latValue = Integer.parseInt(lat.getText().toString());
+                    latValue = Double.parseDouble(lat.getText().toString());
                 }
                 catch (Exception e) {
+                    error.setText(nonIntegerError);
                     error.setVisibility(View.VISIBLE);
                 }
                 try {
-                    lonValue = Integer.parseInt(lon.getText().toString());
+                    lonValue = Double.parseDouble(lon.getText().toString());
                 }
                 catch (Exception e) {
+                    error.setText(nonIntegerError);
                     error.setVisibility(View.VISIBLE);
                 }
-                findUVValue(latValue, lonValue);
+                if (latValue > 90 || latValue < -90) {
+                    error.setText("Error: Latitude must be between -90 and 90");
+                    error.setVisibility(View.VISIBLE);
+                } else if (lonValue > 180 || lonValue < -180) {
+                    error.setText("Error: Longitude must be between -180 and 180");
+                    error.setVisibility(View.VISIBLE);
+                }
+                else {
+                    findUVValue(latValue, lonValue);
+                }
             }
         });
     }
     public void findUVValue(double latitude, double longitude) {
         try {
+            Log.d(LOG, "here");
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
                     Request.Method.GET,
                     "http://api.openweathermap.org/data/2.5/uvi?appid=" + APIKEY + "&lat=" + latitude + "&lon=" + longitude,
@@ -68,7 +83,7 @@ public class MainActivity extends AppCompatActivity {
                     }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        System.out.println(error);
+                        Log.e(LOG, error.toString());
                     }
                 });
             jsonObjectRequest.setShouldCache(false);
@@ -80,9 +95,10 @@ public class MainActivity extends AppCompatActivity {
     void apiCallDone(JSONObject response) {
         try {
             TextView textView = findViewById(R.id.textView);
+            Log.d(LOG,response.get("value").toString());
             textView.setText("UV Risk: " + response.get("value").toString());
         } catch (JSONException e) {
-            System.out.println("JSON error");
+            Log.d(LOG,"JSON error");
         }
     }
 
