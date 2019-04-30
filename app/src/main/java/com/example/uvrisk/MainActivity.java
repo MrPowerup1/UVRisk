bb7f9a0681bbb38140a2bea891e73f9c3f04eb0d
Jonathan Bovee <jbovee99@gmail.com>
2019-04-30 02:03:28 -0500

    Final Version?

--- app/src/main/java/com/example/uvrisk/MainActivity.java
+++ app/src/main/java/com/example/uvrisk/MainActivity.java
@@ -141,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
             textView.setText("LOW: Wear sunglasses on bright days. If you burn easily, cover up and use broad spectrum SPF 30+ sunscreen. Bright surfaces, such as sand, water, and snow, will increase UV exposure.");
         } else if (UV < 5.9) {
             textView.setTextColor(Color.GREEN);
-            textView.setText("MEDIUM: Stay in shade near midday when the sun is strongest. If outdoors, wear sun protective clothing, a wide-brimmed hat, and UV-blocking sunglasses. Generously apply broad spectrum SPF 30+ sunscreen every 2 hours, even on cloudy days, and after swimming or sweating. Bright surfaces, such as sand, water, and snow, will increase UV exposure.");
+            textView.setText("MEDIUM: Stay in shade near midday when the sun is strongest. I    f outdoors, wear sun protective clothing, a wide-brimmed hat, and UV-blocking sunglasses. Generously apply broad spectrum SPF 30+ sunscreen every 2 hours, even on cloudy days, and after swimming or sweating. Bright surfaces, such as sand, water, and snow, will increase UV exposure.");
         } else if (UV < 7.9) {
             textView.setTextColor(Color.RED);
             textView.setText("HIGH: Reduce time in the sun between 10 a.m. and 4 p.m. If outdoors, seek shade and wear sun protective clothing, a wide-brimmed hat, and UV-blocking sunglasses. Generously apply broad spectrum SPF 30+ sunscreen every 2 hours, even on cloudy days, and after swimming or sweating. Bright surfaces, such as sand, water, and snow, will increase UV exposure.");
